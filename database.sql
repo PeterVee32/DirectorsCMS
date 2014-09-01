01cf7432797b68d8dc99b1301d2bdc68545aced6
Peter Vincze <pwnedko1993@gmail.com>
2014-09-01 13:39:53 +0200

    database update

--- database.sql
+++ database.sql
@@ -1,1 +1,25 @@
+CREATE TABLE IF NOT EXISTS `directors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` longtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Sťahujem dáta pre tabuľku `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`) VALUES
+(1, 'pwnedko', '$2y$10$jMGXM0.BVri5JVztT9UTs.kL1Yh7l8Msgznw.nboifx.Hp28PJKFi');
